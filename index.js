33ac523cd7c7b201d16dd24f505b1c833c2bfaf7
muhammad-2002 <masumbilla10104@gmail.com>
2024-05-15 05:40:31 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-const express =require('express')
-const dotenv = require('dotenv')
-dotenv.config()
-const cors =require('cors')
-const port =process.env.PORT || 3000
-const app = express()
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser =require('cookie-parser')
+const port = process.env.PORT || 3000;
 
+const app = express();
 
 //middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://jobseekrex.web.app",
+      "https://jobseekrex.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iwngqer.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iwngqer.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -20,104 +30,153 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
+//my middleware
+const verifyToken =(req,res,next)=>{
+  const token =req.cookies?.token
+  if(!token){
+    return res.status(401).send('unAuthorize')
+  }
+  jwt.verify(token,process.env.JWT_SERECT,(err,decoded)=>{
+    if(err){
+      res.status(401).send('unAuthorize')
+    }
+    req.user = decoded
+    next()
+  })
+
+}
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+app.use(cookieParser())
+
 
 async function run() {
   try {
-   
     const database = client.db("JobSeekerX");
-    const jobCollection = database.collection('Job_Catagories');
-    const ApplyCollection = database.collection('Applyed-job');
+    const jobCollection = database.collection("Job_Catagories");
+    const ApplyCollection = database.collection("Applyed-job");
 
-    app.get('/jobs',async(req,res)=>{
-        const result = await jobCollection.find().toArray()
-        res.send(result)
-    })
+    app.get("/jobs", async (req, res) => {
+      const result = await jobCollection.find().toArray();
+      res.send(result);
+    });
     // Assuming you already have the necessary imports and setup for Express and MongoDB
 
-// Define the route to handle job applications
-app.put('/jobs/:id/apply', async (req, res) => {
-  const jobId = req.params.id;
+    // Define the route to handle job applications
+    app.put("/jobs/:id/apply", async (req, res) => {
+      const jobId = req.params.id;
 
-  try {
-    // Find the job by its ID
-    const job = await jobCollection.findOne({ _id: ObjectId(jobId) });
-
-    if (!job) {
-      return res.status(404).json({ error: 'Job not found' });
-    }
-
-    // Increment the applicants_number field by 1 using the $inc operator
-    const updatedJob = await jobCollection.findOneAndUpdate(
-      { _id: ObjectId(jobId) },
-      { $inc: { applicants_number: 1 } },
-      { returnOriginal: false }
-    );
-
-    // Return the updated job document
-    res.json(updatedJob.value);
-  } catch (error) {
-   
-    res.status(500).json({ error: 'Failed to apply for job' });
-  }
- });
-    app.get('/applied-job',async(req,res)=>{
-        const result = await ApplyCollection.find().toArray()
-        console.log(res)
-        res.send(result)
-    })
-    
-    app.get('/jobs/:id',async(req,res)=>{
-        const job = req.params.id
-        const query = {_id:new ObjectId(job)}
-        const result = await jobCollection.findOne(query)
-      
-        res.send(result)
-    })
-app.delete('/jobs/:id',(req,res)=>{
-  const id = req.params.id
-  const query = {_id:new ObjectId(id)}
-  const result = jobCollection.deleteOne(query)
-  res.send(result)
-})
-    app.get('/my-jobs/:email', async (req, res) => {
       try {
-          const email = req.params.email;
-          const query = { userEmail: email };
-          const cursor = await jobCollection.find(query).toArray();
-          if (cursor.length > 0) {
-              res.send(cursor);
-          } else {
-              res.status(404).send("No items found for the provided email address");
-          }
+        // Find the job by its ID
+        const job = await jobCollection.findOne({ _id: ObjectId(jobId) });
+
+        if (!job) {
+          return res.status(404).json({ error: "Job not found" });
+        }
+
+        // Increment the applicants_number field by 1 using the $inc operator
+        const updatedJob = await jobCollection.findOneAndUpdate(
+          { _id: ObjectId(jobId) },
+          { $inc: { applicants_number: 1 } },
+          { returnOriginal: false }
+        );
+
+        // Return the updated job document
+        res.json(updatedJob.value);
       } catch (error) {
-          console.error(error);
-          res.status(500).send("Internal Server Error");
+        res.status(500).json({ error: "Failed to apply for job" });
       }
-  });
-app.post('/apply-job',async(req,res)=>{
-  const job =req.body
-  const result = await ApplyCollection.insertOne(job)
-  res.send(result)
+    });
+    /// jwt implement
+app.post('/jwt',(req,res)=>{
+  const user = req.body
+  
+  
+  const token =jwt.sign(user,process.env.COOKIE_SERECT,{expiresIn:'1h'})
+  res
+  .cookie(token)
+  .send({status:true})
 
 })
-    app.post('/jobs',async(req,res)=>{
-      const jobs =req.body
-      const result = await jobCollection.insertOne(jobs)
-      res.send(result)
-    })
-    
+
+    app.put("/update/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const updateJob = req.body;
+        const updateDoc = {
+          $set: {
+            ...updateJob,
+          },
+        };
+        const result = await jobCollection.updateOne(
+          filter,
+          updateDoc,
+          options
+        );
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+
+    app.get("/applied-job", async (req, res) => {
+      const result = await ApplyCollection.find().toArray();
+
+      res.send(result);
+    });
+
+    app.get("/jobs/:id", async (req, res) => {
+      const job = req.params.id;
+      const query = { _id: new ObjectId(job) };
+      const result = await jobCollection.findOne(query);
+
+      res.send(result);
+    });
+    app.delete("/jobs/:id", (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = jobCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.get("/my-jobs/:email", async (req, res) => {
+      try {
+        const email = req.params.email;
+        const query = { userEmail: email };
+        const cursor = await jobCollection.find(query).toArray();
+        res.send(cursor);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+    app.post("/apply-job", async (req, res) => {
+      const job = req.body;
+      const result = await ApplyCollection.insertOne(job);
+      res.send(result);
+    });
+    app.post("/jobs", async (req, res) => {
+      const jobs = req.body;
+      const result = await jobCollection.insertOne(jobs);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
- 
-   
   }
 }
 run().catch(console.dir);
 
-
-
-
-app.listen(port,()=>console.log(`listening port${port}`)) 
+app.listen(port, () => console.log(`listening port${port}`));
